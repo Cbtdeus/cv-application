9461c04aeaa668db9bb53a33074ec189732e704f
Cbtdeus <cbtdeus@proton.me>
2024-12-03 05:06:56 +0300

    im doing something stupid, hold on

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,79 @@
-import { useState } from "react"; 
+import { useId, useState } from "react";
 /* import defaultValues from "./defaults"; */
 import ContactInfoForm from "./ContactInfoForm";
 import EducationInfoForm from "./EducationInfoForm";
 import ExperienceInfoForm from "./ExperienceInfoForm";
+import RenderCv from "./renderCv";
 
 export default function App() {
-  function submitEducation(event) {
-    event.preventDefault()
-    setEducation([...education,educationInfo])
-    setEducationInfo({place:"",degree:"",startDate:"",endDate:"",location:""})
+  const [contactInfo, setContactInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
+  const [educationInfo, setEducationInfo] = useState({
+    place: "",
+    degree: "",
+    startDate: "",
+    endDate: "",
+    location: "",
+    id: useId(),
+  });
+  const [education, setEducation] = useState([]);
+  const [experenceInfo, setExperienceInfo] = useState({
+    place: "",
+    position: "",
+    startDate: "",
+    endDate: "",
+    location: "",
+    description: "",
+    id: useId(),
+  });
+  const [experience, setExperience] = useState([]);
+
+  function SubmitEducation(event) {
+    event.preventDefault();
+    setEducation([...education, educationInfo]);
+    setEducationInfo({
+      place: "",
+      degree: "",
+      startDate: "",
+      endDate: "",
+      location: "",
+      id: useId(),
+    });
   }
 
-  function submitExperience(event) {
-    event.preventDefault()
-    setExperience([...experience,experenceInfo])
-    setExperienceInfo({place:"",position:"",startDate:"",endDate:"",location:"",description:""})
+  function SubmitExperience(event) {
+    event.preventDefault();
+    setExperience([...experience, experenceInfo]);
+    setExperienceInfo({
+      place: "",
+      position: "",
+      startDate: "",
+      endDate: "",
+      location: "",
+      description: "",
+      id: useId(),
+    });
   }
 
-  const [contactInfo,setContactInfo] = useState({name:"",email:"",phone:"",address:""})
-  const [educationInfo,setEducationInfo] = useState({place:"",degree:"",startDate:"",endDate:"",location:""})
-  const [education,setEducation] = useState([])
-  const [experenceInfo,setExperienceInfo] = useState({place:"",position:"",startDate:"",endDate:"",location:"",description:""})
-  const [experience,setExperience] = useState([])
-
-  return <>
-  <ContactInfoForm data={contactInfo} setter={setContactInfo} />
-  <EducationInfoForm data={educationInfo} setter={setEducationInfo}
-   onSubmit={submitEducation}/>
-  <ExperienceInfoForm data={experenceInfo} setter={setExperienceInfo}
-   onSubmit={submitExperience}/>
-  </>
+  return (
+    <>
+      <ContactInfoForm data={contactInfo} setter={setContactInfo} />
+      <EducationInfoForm
+        data={educationInfo}
+        setter={setEducationInfo}
+        onSubmit={SubmitEducation}
+      />
+      <ExperienceInfoForm
+        data={experenceInfo}
+        setter={setExperienceInfo}
+        onSubmit={SubmitExperience}
+      />
+      <RenderCv contactInfo={contactInfo} />
+    </>
+  );
+  /* render already filled out education/exp forms and allow their editing */
 }
--- src/EducationInfoForm.jsx
+++ src/EducationInfoForm.jsx
@@ -1,15 +1,43 @@
-import InputField from "./InputField"
+import { useState } from "react";
+import InputField from "./InputField";
 
-export default function EducationInfoForm({data,setter,onSubmit}) {
+export default function EducationInfoForm({ data, setter, onSubmit }) {
+  const [isActive, setActive] = useState(false);
 
-    return <form onSubmit={onSubmit}>
-    <InputField label="Institution" type="text" value={data.place} onChange={
-            (e) => setter({...data,place:e.target.value})} />
-    <InputField label="Degree" type="text" value={data.degree} onChange={
-            (e) => setter({...data,degree:e.target.value})} />  
-            {/* DateFields go here */}
-    <InputField label="Location" type="text" value={data.location} onChange={
-            (e) => setter({...data,location:e.target.value})} /> 
-            <input type="submit" value="Save" />
-    </form>
+  return (
+    <>
+      {isActive ? (
+        <form
+          className="education-form"
+          onSubmit={(e) => {
+            onSubmit(e), setActive(false);
+          }}
+        >
+          <InputField
+            label="Institution"
+            type="text"
+            value={data.place}
+            onChange={(e) => setter({ ...data, place: e.target.value })}
+          />
+          <InputField
+            label="Degree"
+            type="text"
+            value={data.degree}
+            onChange={(e) => setter({ ...data, degree: e.target.value })}
+          />
+
+          <InputField
+            label="Location"
+            type="text"
+            value={data.location}
+            onChange={(e) => setter({ ...data, location: e.target.value })}
+          />
+          <button onClick={() => setActive(false)}>Cancel</button>
+          <input type="submit" value="Save" />
+        </form>
+      ) : (
+        <button onClick={() => setActive(true)}>Add</button>
+      )}
+    </>
+  );
 }
